c09c743823d2d6b2c1041bbb5e3e72a034d1d9b5
p-adrian05 <adrian16616@gmail.com>
2020-05-01 15:13:07 +0200

    Created CardApi class in Model package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,8 @@ module blackJackFX {
     requires javafx.controls;
     requires javafx.fxml;
     requires lombok;
+    requires com.google.gson;
+    requires org.apache.commons.io;
 
     opens blackJackFX to javafx.fxml;
     exports blackJackFX;
