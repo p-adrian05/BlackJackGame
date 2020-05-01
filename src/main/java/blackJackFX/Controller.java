fc4b59d9c4aec9fa8e26789a43798baed5703431
p-adrian05 <adrian16616@gmail.com>
2020-05-01 13:37:09 +0200

    Rename LoginController to Controller

--- src/main/java/blackJackFX/Controller.java
+++ src/main/java/blackJackFX/Controller.java
@@ -5,13 +5,9 @@ import java.io.IOException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
-public class LoginController {
+public class Controller {
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        Main.setRoot("secondary");
-    }
-
     public void loginBtnClicked(ActionEvent actionEvent) {
     }
 }
