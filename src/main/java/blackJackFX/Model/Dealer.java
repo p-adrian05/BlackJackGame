b32b06fd7682d88d16d42690acbcf5e5fc4cb1d0
p-adrian05 <adrian16616@gmail.com>
2020-05-01 15:14:51 +0200

    Created Dealer class in Model package

--- src/main/java/blackJackFX/Model/Dealer.java
+++ src/main/java/blackJackFX/Model/Dealer.java
@@ -1,14 +1,13 @@
 package blackJackFX.Model;
 
+
+import lombok.Data;
+
 import java.util.List;
-import lombok.*;
 
 @Data
-public class Player {
+public class Dealer {
 
-    private int funds;
-    private int bet;
-    private List<Card> splitCards;
     private int cardsSumValues;
     private List<Card> cards;
 
