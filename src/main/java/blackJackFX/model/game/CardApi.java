af5f6b2ad27be1ad0833f31fb763ae7cb87e780d
p-adrian05 <adrian16616@gmail.com>
2020-05-13 10:16:38 +0200

    Added javadoc to CardApi interface

--- src/main/java/blackJackFX/model/game/CardApi.java
+++ src/main/java/blackJackFX/model/game/CardApi.java
@@ -1,5 +1,9 @@
 package blackJackFX.model.game;
 
+/**
+ * The CardApi interface provides to get an implementation of a {@link Deck} interface.
+ */
+
 public interface CardApi {
 
      Deck getDeck();
