56d3b8a11902bd9241405054d4196a9ad10dc613
p-adrian05 <adrian16616@gmail.com>
2020-05-13 09:47:59 +0200

    Fixed javadoc in DeckFranceCards class

--- src/main/java/blackJackFX/model/game/DeckFranceCards.java
+++ src/main/java/blackJackFX/model/game/DeckFranceCards.java
@@ -4,13 +4,13 @@ import java.util.LinkedList;
 import java.util.List;
 
 /**
- * Class represents an implementation of {@code Deck} interface.
+ * Class represents an implementation of {@link Deck} interface.
  */
 
 public class DeckFranceCards implements Deck{
 
     /**
-     * The list of {@code FranceCard} objects.
+     * The list of {@link FranceCard} objects.
      */
     private LinkedList<FranceCard> cards;
 
@@ -19,15 +19,20 @@ public class DeckFranceCards implements Deck{
         return new LinkedList<>(this.cards);
     }
 
+    /**
+     * Returns a {@link Card} object from the {@link #cards} list and remove it.
+     *
+     * @return a {@link Card} object
+     */
     @Override
     public Card getCard() {
         return this.cards.removeFirst();
     }
 
     /**
-     * Calculates the summary value of a list of {@code Card} objects.
+     * Calculates the summary value of a list of {@link Card} objects.
      *
-     * @param cards the list of {@code Card} objects
+     * @param cards the list of {@link Card} objects
      * @return a summary value
      */
     @Override
@@ -42,9 +47,6 @@ public class DeckFranceCards implements Deck{
         }
         return sum;
     }
-
-
-
 }
 
 
