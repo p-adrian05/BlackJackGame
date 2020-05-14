497a77f9a26de287f1f1436d85b9642b3e887532
p-adrian05 <adrian16616@gmail.com>
2020-05-14 12:31:12 +0200

    Added JUnit test

---
+++ src/main/test/blackJack/model/game/GameUtilsTest.java
@@ -0,0 +1,35 @@
+package blackJack.model.game;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GameUtilsTest {
+
+    @Test
+    void testCalculateResult() {
+    }
+
+    @Test
+    void testCalculateResult2() {
+    }
+
+    @Test
+    void testCalculatePrize() {
+    }
+
+    @Test
+    void testCalculatePrize2() {
+    }
+
+    @Test
+    void testValidateBet() {
+    }
+
+    @Test
+    void testConvertResult() {
+    }
+
+    @Test
+    void testMadeStringResult() {
+    }
+}
