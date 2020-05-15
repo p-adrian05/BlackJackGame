8ff706f88fac166e1d93ed0f73a828c32804980e
p-adrian05 <adrian16616@gmail.com>
2020-05-15 11:28:49 +0200

    Made GameResult class

--- src/main/java/blackJack/results/GameResult.java
+++ src/main/java/blackJack/results/GameResult.java
@@ -1,5 +1,53 @@
 package blackJack.results;
 
-public class GameResult {
+import lombok.*;
 
+import javax.persistence.*;
+import java.time.ZonedDateTime;
+
+/**
+ * Class representing the result of a game played by a specific player.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class GameResult {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false,  unique=true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private int funds;
+
+    @Column(nullable = false)
+    private int lostMoney;
+
+    @Column(nullable = false)
+    private int wonMoney;
+
+    @Column(nullable = false)
+    private int wonCount;
+
+    @Column(nullable = false)
+    private int loseCount;
+
+    @Version
+    @Setter(AccessLevel.NONE)
+    private long version;
+
+    @Column(nullable = false)
+    private ZonedDateTime created;
+
+    @PrePersist
+    protected void onPersist() {
+        created = ZonedDateTime.now();
+    }
 }
