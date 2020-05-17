37e3c2cf14cacce743c80aa2f6a4aa71f4df2192
p-adrian05 <adrian16616@gmail.com>
2020-05-17 14:31:15 +0200

    Added javadoc to UserDao class

--- src/main/java/blackJack/results/UserDao.java
+++ src/main/java/blackJack/results/UserDao.java
@@ -21,7 +21,17 @@ public class UserDao extends GenericJpaDao<User> {
         }
         return instance;
     }
-    public Optional<User> findbyUsername(String username) {
+
+    /**
+     * Returns a {@link User} entity instance with the specified username from the
+     * database. The method returns an empty {@link Optional} object when
+     * the instance does not exists.
+     *
+     * @param username the String name to look for
+     * @return an {@link Optional} object wrapping the {@link User} instance with
+     * the specified primary key
+     */
+    public Optional<User> findByUsername(String username) {
         try {
             return Optional.of(entityManager.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class)
                     .setParameter("username", username).getSingleResult());
